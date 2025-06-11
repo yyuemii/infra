1a271879da2e374b7c3301b5129fbe3f1c211c4e
brianna <yyuemii@gmail.com>
2025-06-10 22:45:02 -0400

    flake: switch to flake-parts, add opnix to inputs and overlays

--- flake.nix
+++ flake.nix
@@ -4,20 +4,54 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    opnix.url = "github:brizzbuzz/opnix";
   };
 
   outputs =
-    {
-      ...
-    }@inputs:
-    let
-      hosts = import ./hosts { inherit inputs; };
-    in
-    {
-      inherit (hosts) nixosConfigurations;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # all development devices im using are aarch64 based
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      flake =
+        let
+          hosts = import ./hosts { inherit inputs; };
+        in
+        {
+          inherit (hosts) nixosConfigurations;
+        };
+
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+
+            overlays = [
+              inputs.opnix.overlays.default
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              opnix
+            ];
+          };
+        };
     };
 }
